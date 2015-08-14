067b79acf7bbd6fbed5abfc5d4de98eab258ff73
Pierrick Koch <pierrick.koch@gmail.com>
2015-08-14 17:20:29 +0200

    ascii bag use with statement

--- ascii_bag.py
+++ ascii_bag.py
@@ -15,26 +15,22 @@ from sensor_msgs.msg import PointField
 
 begin   = int(sys.argv[1])
 end     = int(sys.argv[2])
-frame   = str(sys.argv[3])
-name    = str(sys.argv[4])
+name    = str(sys.argv[3])
+topic   = '/%s/velodyne' % name
 
 fields  = [PointField('x', 0, PointField.FLOAT32, 1),
            PointField('y', 4, PointField.FLOAT32, 1),
            PointField('z', 8, PointField.FLOAT32, 1),
            PointField('intensity', 12, PointField.FLOAT32, 1)]
 path    = "cloud.%05i.txt"
-bag     = rosbag.Bag(name, 'w')
 
-try:
+with rosbag.Bag(name, 'w') as bag:
     for seq in range(begin, end+1):
         with open(path%seq) as f:
             lines = f.readlines()
 
         points  = [map(float, line.split()) for line in lines]
         stamp   = rospy.Time.from_sec(time.time() + seq) # FIXME
-        header  = Header(seq=seq, stamp=stamp, frame_id=frame)
+        header  = Header(seq=seq, stamp=stamp, frame_id=topic)
         pc2     = point_cloud2.create_cloud(header, fields, points)
-        bag.write(frame, pc2, stamp)
-
-finally:
-    bag.close()
+        bag.write(topic, pc2, stamp)
